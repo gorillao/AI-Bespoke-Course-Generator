d426797144d1e12610be4f01cab3ff0d019545c2
gorillao <157346418+gorillao@users.noreply.github.com>
2024-03-28 09:07:44 +0000

    PR fix

--- src/app/create/[courseId]/page.tsx
+++ src/app/create/[courseId]/page.tsx
@@ -14,4 +14,27 @@ type Props = {
 const CreateChapters = async ({params:{ courseId}}: Props) => {
     const session = await getAuthSession()
     if (!session?.user){
-        return redirect('/gallery
+        return redirect('/gallery')
+    }
+    const course = await prisma.course.findUnique({
+        where:{
+            id:courseId,
+
+        },
+        include:{
+            units:{
+                include:{
+                    chapters:true
+                },
+            },
+        },
+        
+})
+if (!course){
+    return redirect('/create')
+}
+return (
+    <div className="flex flex-col items-start max-w-xl mx-auto my-16"> 
+    <h5 className='text-sm uppercase text-secondary-foreground/60'>
+        Course Name
+ 
