4d9983aa9ca7accf25a6b8b71d93726496e4ae8b
gorillao <157346418+gorillao@users.noreply.github.com>
2024-04-05 14:48:16 +0000

    libata: Prevent interface errors with Seagate FreeAgent GoFlex

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -23,3 +23,17 @@ const UserAccountNav = ({user}: Props) => {
             <UserAvatar user={user}/>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
+            <div className='flex items-center justify-start gap-2 p-2'>
+                <div className='flex flex-col space-y-1 leading-none'>
+
+                </div>
+                {user?.name && (<p className='font-medium'>{user.name}</p>)}
+                {user?.email && (<p className='w-{200px} truncate text-sm text-secondary-foreground'>{user.email}</p>)}
+            </div>
+            <DropdownMenuSeparator/>
+            <DropdownMenuItem onSelect={()=>{
+                signOut();
+
+
+            }}
+            className='tex
