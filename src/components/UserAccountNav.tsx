85bc9b3adb4dd6aacfbb5fa892b36f5c2e9e7e7a
gorillao <157346418+gorillao@users.noreply.github.com>
2024-04-05 08:53:37 +0000

    Version bump to 0.3.4

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -3,4 +3,23 @@
 import React from 'react'
 import { Button } from './ui/button'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
-//import { UserInfo } from '
+//import { UserInfo } from 'os'
+//import { AdapterUser } from 'next-auth/adapters'
+import { User } from 'next-auth'
+import { signOut } from 'next-auth/react'
+import { LogOut } from 'lucide-react'
+import UserAvatar from './UserAvatar'
+//import { userAgent } from 'next/server'
+
+
+type Props = {
+    user:User
+}
+
+const UserAccountNav = ({user}: Props) => {
+  return (
+    <DropdownMenu>
+        <DropdownMenuTrigger>
+            <UserAvatar user={user}/>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
