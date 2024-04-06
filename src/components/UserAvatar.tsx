c124482b5dc916007d3527085e1ba130129b3f38
gorillao <157346418+gorillao@users.noreply.github.com>
2024-04-06 16:28:55 +0000

    - package version

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -11,4 +11,17 @@ const UserAvatar = ({user}: Props) => {
   return (
     <Avatar>
         {user.image?(
-            <div className='relative w-full h-full aspect-sq
+            <div className='relative w-full h-full aspect-square'>
+                <Image fill src={user.image} alt="user profile" referrerPolicy='no-referrer'/>
+
+            </div>
+        ):(
+            <AvatarFallback>
+                <span className='sr-only'>{user?.name}</span>
+            </AvatarFallback>
+        )}
+    </Avatar>
+  )
+}
+
+export default UserAvatar
