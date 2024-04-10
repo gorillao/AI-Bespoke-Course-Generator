cd07fe1915ef539784226f50d87b2f939ddd7ca0
gorillao <157346418+gorillao@users.noreply.github.com>
2024-04-10 06:00:02 +0000

    Bumped version numbers.

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -62,4 +62,26 @@ const useFormField = () => {
   }
 }
 
-type FormItemContextValue 
+type FormItemContextValue = {
+  id: string
+}
+
+const FormItemContext = React.createContext<FormItemContextValue>(
+  {} as FormItemContextValue
+)
+
+const FormItem = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => {
+  const id = React.useId()
+
+  return (
+    <FormItemContext.Provider value={{ id }}>
+      <div ref={ref} className={cn("space-y-2", className)} {...props} />
+    </FormItemContext.Provider>
+  )
+})
+FormItem.displayName = "FormItem"
+
+c
