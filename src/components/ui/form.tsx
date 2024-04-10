fe5f792eb365aaaf39c384e424b0330ea4530115
gorillao <157346418+gorillao@users.noreply.github.com>
2024-04-10 07:41:56 +0000

    Fix variable

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -84,4 +84,21 @@ const FormItem = React.forwardRef<
 })
 FormItem.displayName = "FormItem"
 
-c
+const FormLabel = React.forwardRef<
+  React.ElementRef<typeof LabelPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
+>(({ className, ...props }, ref) => {
+  const { error, formItemId } = useFormField()
+
+  return (
+    <Label
+      ref={ref}
+      className={cn(error && "text-destructive", className)}
+      htmlFor={formItemId}
+      {...props}
+    />
+  )
+})
+FormLabel.displayName = "FormLabel"
+
+const FormControl = React.forwardRef<
