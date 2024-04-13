ffc50f7ecbc19e10c9a0400c0a5e1f0de05416ca
gorillao <157346418+gorillao@users.noreply.github.com>
2024-04-13 07:48:38 +0000

    Update bootstrap.R

---
+++ src/lib/db.ts
@@ -0,0 +1,13 @@
+
+import { PrismaClient } from "@prisma/client";
+import "server-only";
+ 
+declare global {
+  // eslint-disable-next-line no-var, no-unused-vars
+  var cachedPrisma: PrismaClient;
+}
+ 
+export let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
