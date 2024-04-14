6c89e843f119224295288576727bc55ce9c0b394
gorillao <157346418+gorillao@users.noreply.github.com>
2024-04-14 09:13:55 +0000

    Fixes for windows

--- src/lib/youtube.ts
+++ src/lib/youtube.ts
@@ -6,4 +6,21 @@ import { strict_output } from "./gpt";
 export async function searchYoutube(searchQuery:string){
     searchQuery=encodeURIComponent(searchQuery);
     const {data}=await axios.get(
-        `https://www.googleapis.com/youtube/v3/search?key=$
+        `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&q=${searchQuery}&videoDuration=medium&videoEmbeddable=true&type=video&maxResults=5`
+    )
+    if (!data){
+        console.log('youtube fail')
+        return null
+    }
+    if (data.items[0]==undefined){
+        console.log('youtube fail')
+        return null
+    }
+    return data.items[0].id.videoId;
+}
+export async function getTranscript(videoId:string){
+    try{
+        let transcript_arr=await YoutubeTranscript.fetchTranscript(videoId,{
+            lang: "en",
+            country:"EN"
+       
