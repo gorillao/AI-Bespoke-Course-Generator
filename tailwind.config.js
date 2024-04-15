cbf86359872c9b1ba4d1b87e68ad2727d7640faf
gorillao <157346418+gorillao@users.noreply.github.com>
2024-04-15 08:13:26 +0000

    Prepare 0.1.8 release

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,4 +9,22 @@ module.exports = {
 	],
   theme: {
     container: {
-    
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+       
