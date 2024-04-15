ea84cd5aa6e0e4ccaecec7a029f2d5d6b86db262
gorillao <157346418+gorillao@users.noreply.github.com>
2024-04-15 19:52:58 +0000

    Fix font

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,4 +27,17 @@ module.exports = {
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
-       
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foregr
